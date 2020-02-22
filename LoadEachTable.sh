a7e1bd99266118fab8837490d9623d9dfbe5f58a
Madhav Sodhani <madhav2302@gmail.com>
2020-02-22 10:38:53 -0500

    Script to load each table and sync to VM

---
+++ LoadEachTable.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+make a1test.out
+for i in 1 2 3 4 5 6 7; do
+  printf "1\n${i}\n" | ./a1test.out
+done
---
+++ SyncProjectWithVM.sh
@@ -0,0 +1,37 @@
+#make clean
+echo "$#"
+
+PARAM="-anv"
+
+if [ "$#" -eq 1 ]; then
+  PARAM="-av"
+fi
+
+rsync ${PARAM} \
+  --exclude='.git' \
+  --exclude='cmake-build-debug' \
+  --exclude='.idea' \
+  --exclude='*.bin' \
+  --exclude='*.o' \
+  --exclude='*.bin.bigq' \
+  --exclude='*.out' \
+  --exclude='lex.yy.c' \
+  --exclude='y.tab.*' \
+  --exclude='main' \
+  ./ msodhani@storm.cise.ufl.edu:~/database_implementation/
+#make main
+#make a1test.out
+#make test.out
+
+echo ""
+echo ""
+echo ""
+if [ "$#" -eq 1 ]; then
+  echo "############################################################"
+  echo "Sync completed"
+  echo "############################################################"
+else
+  echo "############################################################"
+  echo "This is only diff, pass 1 argument to complete the sync"
+  echo "############################################################"
+fi

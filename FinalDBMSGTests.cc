bf3fb83d2a1ae4d476b0fde5699828c084ebd4dd
Madhav Sodhani <madhav2302@gmail.com>
2020-04-28 20:25:47 -0400

    Fix GTests

--- FinalDBMSGTests.cc
+++ FinalDBMSGTests.cc
@@ -1,34 +1,61 @@
 #include "gtest/gtest.h"
 #include "Database.h"
-#include "Schema.h"
 #include "ParseTree.h"
-#include "QueryPlanner.h"
+#include <fstream>
+#include <iostream>
 
-extern "C" {
-    int yyparse(void);   // defined in y.tab.c
-    struct YY_BUFFER_STATE *yy_scan_string(const char *);
+using namespace std;
+
+bool fileExists(std::string fileName) {
+    ifstream f(fileName.c_str());
+    return f.good();
 }
 
-extern struct TableList *tables; // the list of tables and aliases in the query
-extern struct ColumnList *myColumns; //
-extern struct NameList *orderAtts; //
 
 TEST(DBMSTest, CreateTable){
-    Database db;
-    char *input = "CREATE TABLE mytable (att1 INTEGER, att2 DOUBLE, att3 STRING) AS HEAP";
-    yy_scan_string(input);
-    cout<<"Table Name "<<tables->tableName;
-    cout<<"Columns "<<myColumns;
-    cout<<"OrderAtts"<<orderAtts;
-    db.Create(tables->tableName, myColumns, orderAtts);
+char *tableName = "test_table";
+ColumnList column1;
+column1.name = "att1";
+column1.type = "INTEGER";
 
+ColumnList column2;
+column2.name = "att2";
+column2.type = "DOUBLE";
+
+ColumnList column3;
+column3.name = "att3";
+column3.type = "STRING";
+
+column1.next = &column2;
+column2.next = &column3;
+column3.next = nullptr;
+Database db;
+
+db.Create(tableName, &column1, nullptr);
+
+EXPECT_EQ(fileExists("schema/test_table"), true);
+EXPECT_EQ(fileExists("data/test_table.bin"), true);
+EXPECT_EQ(fileExists("data/test_table.bin.metadata"), true);
+
+remove("schema/test_table");
+remove("data/test_table.bin");
+remove("data/test_table.bin.metadata");
 }
 
-TEST(DBMSTest, DropTable){
-    Database db;
-    char *input = "DROP TABLE mytable";
-    yy_scan_string(input);
-    db.Drop(tables->tableName);
+TEST(DBMSTest, DropTable) {
+char *tableName = "test_table";
+ColumnList column1;
+column1.name = "att1";
+column1.type = "INTEGER";
+column1.next = nullptr;
+
+Database db;
+db.Create(tableName, &column1, nullptr);
+db.Drop(tableName);
+
+EXPECT_EQ(fileExists("schema/test_table"), false);
+EXPECT_EQ(fileExists("data/test_table.bin"), false);
+EXPECT_EQ(fileExists("data/test_table.bin.metadata"), false);
 }
 
 
